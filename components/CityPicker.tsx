fd7c00f01c03f10abcf5e19ca7aa57208a45ec20
Robert <robertmaximus383@gmail.com>
2023-07-08 13:59:49 +0300

    HeroIcons pack added and style for Country label

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -4,6 +4,8 @@ import React from 'react'
 import Select from 'react-select'
 import { Country, City, State } from "country-state-city"
 import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { GlobeIcon } from '@heroicons/react/solid'
 
 type option = {
     value: {
@@ -49,11 +51,29 @@ const CityPicker = () => {
     const [selectedCountry, setSelectedCountry] = useState<option>(null)
     const [selectedState, setSelectedState] = useState<stateOption>(null)
     const [selectedCity, setSelectedCity] = useState<cityOption>(null)
+
+    const router = useRouter();
+
+    const handleSelectedCountry = (option: option) => {
+        setSelectedCountry(option)
+        setSelectedState(null);
+        setSelectedCity(null);
+    };
+
   return (
-    <div>
-        <Select 
-            options={options} 
-        />
+    <div className='space-y-4'>
+        <div className='space-y-2'>
+            <div className='flex items-center space-x-2 text-white/80'>
+                <GlobeIcon className='h-5 w-5 text-white'/>
+                <label htmlFor="country">Country</label>
+            </div>
+            <Select
+                className='text-black'
+                value={selectedCountry}
+                onChange={handleSelectedCountry}
+                options={options} 
+            />
+        </div>
     </div>
   )
 }
