425c1eafded6f7ecd9e4a643f31d3f82684617d7
Robert <robertmaximus383@gmail.com>
2023-07-09 12:27:34 +0300

    query definition fixed

--- graphql/queries/fetchWeatherQueries.ts
+++ graphql/queries/fetchWeatherQueries.ts
@@ -3,8 +3,8 @@ import {gql} from "@apollo/client"
 const fetchWeatherQuery = gql`
   query MyQuery(
     $current_weather: String
-    $daily: String = "weathercode, temperature_2m_max, temperature_2m_min, apparent_temperature_max, apparent_temperature_min, sunrise, sunset, uv_index_max, uv_index_clear_sky_max"
-    $hourly: String = "temperature_2m, relativehumidity_2m, apparent_temperature, precipitation_probability, precipitation, rain, showers, snowfall, snow_depth, windgusts_10m, uv_index, uv_index_clear_sky"
+    $daily: String = "weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max,uv_index_clear_sky_max"
+    $hourly: String = "temperature_2m,relativehumidity_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,windgusts_10m,uv_index,uv_index_clear_sky"
     $latitude: String!
     $longitude: String!
     $timezone: String!
