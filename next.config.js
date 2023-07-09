8516106c191eba70f38a1b719c1deadc6a196354
Robert <robertmaximus383@gmail.com>
2023-07-09 15:58:29 +0300

    config updated with weatherbit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images:{
+    domains: ['www.weatherbit.io']
+  },
   experimental: {
     appDir: true,
-    // serverComponentsExternalPackages: ['@tremor/react'],
+    serverComponentsExternalPackages: ['@tremor/react'],
   },
 }
 
