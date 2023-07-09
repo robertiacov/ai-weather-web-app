421aa62175ac289fb66dc794021850e5e90d450f
Robert <robertmaximus383@gmail.com>
2023-07-09 11:06:06 +0300

    typing i for Daily

--- typings.d.ts
+++ typings.d.ts
@@ -6,6 +6,19 @@ interface CurrentWeather {
     windspeed: number;
 }
 
+interface Daily {
+    apparent_temperature_max: [number]
+    apparent_temperature_min: [number]
+    sunrise: [string]
+    sunset: [string]
+    temperature_2m_max: [number]
+    temperature_2m_min: [number]
+    time: [string]
+    uv_index_clear_sky_max: [number]
+    uv_index_max: [number]
+    weathercode: [number]
+}
+
 interface DailyUnits {
     apparent_temperature_max: string;
     apparent_temperature_min: string;
