8ec8b8576f8d3ccb656db4e721ced336ba623427
Robert <robertmaximus383@gmail.com>
2023-07-09 12:40:13 +0300

    city page first design update

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -28,7 +28,27 @@ async function WeatherPage({params: {city, lat, long}}: Props) {
     console.log(results);
 
   return (
-    <div>Welcome to the weather page: {city} {lat} {long}</div>
+    <div>
+        {/* <InformationPanel /> */}
+
+
+        <div>
+            <div className='p-5'>
+                <div className='pb-5'>
+                    <h2 className='text-xl font-bold'>Todays Overview</h2>
+                    <p className='text-sm text-gray-400'>Last Updated At: {" "}
+                        {new Date(results.current_weather.time).toLocaleString()} ({results.timezone})
+                    </p>
+                </div>
+
+            <div>
+
+            </div>
+            {/* CalloutCard */}
+            </div>
+
+        </div>
+    </div>
   )
 }
 
