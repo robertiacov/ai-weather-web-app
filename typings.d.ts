e3ab80f11b0dcea199debf77b538f244341cb299
Robert <robertmaximus383@gmail.com>
2023-07-09 11:08:04 +0300

    typing i for Hourly

--- typings.d.ts
+++ typings.d.ts
@@ -7,16 +7,16 @@ interface CurrentWeather {
 }
 
 interface Daily {
-    apparent_temperature_max: [number]
-    apparent_temperature_min: [number]
-    sunrise: [string]
-    sunset: [string]
-    temperature_2m_max: [number]
-    temperature_2m_min: [number]
-    time: [string]
-    uv_index_clear_sky_max: [number]
-    uv_index_max: [number]
-    weathercode: [number]
+    apparent_temperature_max: [number];
+    apparent_temperature_min: [number];
+    sunrise: [string];
+    sunset: [string];
+    temperature_2m_max: [number];
+    temperature_2m_min: [number];
+    time: [string];
+    uv_index_clear_sky_max: [number];
+    uv_index_max: [number];
+    weathercode: [number];
 }
 
 interface DailyUnits {
@@ -30,4 +30,20 @@ interface DailyUnits {
     uv_index_clear_sky_max: string;
     uv_index_max: string;
     weathercode: string;
+}
+
+interface Hourly{
+    apparent_temperature: [number];
+    precipitation: [number];
+    precipitation_probability: [number];
+    rain: [number];
+    relativehumidity_2m: [number];
+    showers: [number];
+    snow_depth: [number];
+    snowfall: [number];
+    temperature_2m: [number];
+    time: [string];
+    uv_index: [number];
+    uv_index_clear_sky: [number];
+    windgusts_10m: [number];
 }
